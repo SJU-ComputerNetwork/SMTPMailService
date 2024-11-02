6e113204564767dfd3d0e7f2ac7df012f540a4ee
권경재 <gweongyeongjae1@gmail.com>
2024-11-02 22:51:47 +0900

    Update ReceiverPanel.java

--- src/ReceiverPanel.java
+++ src/ReceiverPanel.java
@@ -34,7 +34,7 @@ public class ReceiverPanel extends JPanel{
 		changeMod.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				mailAppClient.changeMod();
+				mailAppClient.showContentPanel();
 			}
 		});
 		add(changeMod);
